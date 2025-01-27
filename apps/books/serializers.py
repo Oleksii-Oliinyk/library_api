d3cb8a0eac9fa1776b3d6514c8d39523af51ecb6
Oleksii-Oliinyk <oleksii.oliinyk.work@gmail.com>
2025-01-27 14:47:14 +0200

    added validators to book serializer

--- apps/books/serializers.py
+++ apps/books/serializers.py
@@ -6,3 +6,15 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = '__all__'
         
+        
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError('Name and description cannot be the same')
+        else:
+            return data 
+        
+    
+    def validate_name(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError('Name is too short')
+        return value
