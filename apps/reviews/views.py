6ec0587eec44b30c5331d501b3e6c79574da91ae
Oleksii-Oliinyk <oleksii.oliinyk.work@gmail.com>
2025-01-29 13:35:25 +0200

    changed permissions

--- apps/reviews/views.py
+++ apps/reviews/views.py
@@ -1,14 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from apps.reviews.models import Review
 from apps.reviews.serializers import ReviewSerializer
 from apps.reviews.permissions import IsReviewUserOrReadOnly
 
 class ReviewListAV(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     
     def get(self, request, id):
         reviews = Review.objects.filter(book = id)
